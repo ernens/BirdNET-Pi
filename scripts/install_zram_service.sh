d5cb46b44140e4c74cfe16421c46ad4dc98fc9a7
Björn Ernens <93145624+ernens@users.noreply.github.com>
2023-10-08 09:19:59 +0200

    Update install_zram_service.sh
    Dynamically determines the size of ZRAM device based on the available system memory.
    It uses a ternary conditional statement to assess whether half of the available system memory is less than 2048 megabytes.
    If it is, it sets the ZRAM size to 2048 megabytes; otherwise, it allocates a ZRAM size equal to half of the available system memory.
    This approach has the potential to enhance the performance of memory-intensive tasks and reduce the dependence on slower storage devices, such as SD cards or SSDs, for swap space.
    Conversely, on systems with limited memory, it prevents excessive allocation of ZRAM, thereby averting potential issues related to memory contention.

--- scripts/install_zram_service.sh
+++ scripts/install_zram_service.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-echo "Configuring zram.service"
+total_mem=$(free -m | awk '/^Mem:/{print $2}')
+zram_size=$((total_mem / 2 < 2048 ? 2048 : total_mem / 2))
+echo "Configuring zram.service with size: ${zram_size}M"
+# Create zram configuration
 sudo touch /etc/modules-load.d/zram.conf
 echo 'zram' | sudo tee /etc/modules-load.d/zram.conf
 sudo touch /etc/modprobe.d/zram.conf
-echo 'options zram num_devices=1' | sudo tee /etc/modprobe.d/zram.conf
+echo "options zram num_devices=1" | sudo tee /etc/modprobe.d/zram.conf
 sudo touch /etc/udev/rules.d/99-zram.rules
-echo 'KERNEL=="zram0", ATTR{disksize}="2G",TAG+="systemd"' \
+echo "KERNEL==\"zram0\", ATTR{disksize}=\"${zram_size}M\", TAG+=\"systemd\"" \
   | sudo tee /etc/udev/rules.d/99-zram.rules
 sudo touch /etc/systemd/system/zram.service
 echo "Installing zram.service"
